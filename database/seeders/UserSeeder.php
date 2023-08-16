c8701c100f2bd9094bffa7ae431a5135b4ce2ab7
cperdomo12cr <cperdomo@arcetec.com.co>
2023-08-15 21:15:57 -0500

    feat: creando usuario para jwt

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name' => 'unknow',
+                'email' => 'unknow@gmail.com',
+                'password' => '12345678',
+            ]
+        ];
+
+
+        foreach ($users as $user) {
+            User::create([
+                'name' => $user['name'],
+                'email' => $user['email'],
+                'password' => Hash::make($user['password'])
+            ]);
+        }
+    }
+}
